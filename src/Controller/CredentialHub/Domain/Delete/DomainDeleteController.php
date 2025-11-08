6edac519a40f867d62dad7f5450ef3576f05378d
boszormenyirobert <boszormenyirobert@yahoo.com>
2025-11-08 01:53:15 +0100

    refact domain read

--- src/Controller/CredentialHub/Domain/Delete/DomainDeleteController.php
+++ src/Controller/CredentialHub/Domain/Delete/DomainDeleteController.php
@@ -95,7 +95,7 @@ class DomainDeleteController extends AbstractController
             if(!$process) {
                 return $this->responseHelper->createErrorResponse('Invalid or missing processId');
             }
-
+            $this->logger->critical('DomainDeleteController: domainDeleteCredential processId '.$process );
             $response = $domainDeleteService->deleteDomain($process);
             
             return $this->json([

0773200726a6268cbc1335ca39527f6da724b209
boszormenyirobert <boszormenyirobert@yahoo.com>
2025-11-10 01:35:55 +0100

    bugfix

--- src/Service/AuthBridge/AuthBridgeHandler/Domain/Encryptor.php
+++ src/Service/AuthBridge/AuthBridgeHandler/Domain/Encryptor.php
@@ -92,6 +92,7 @@ class Encryptor
         $iv = base64_decode($authBridge->getIv());
         $dbEncryptedCredentials = [];
         foreach ($credentialsCollection as $credentialData) {
+            $this->logger->critical('The user email: ' . $user['email']);
             $encryptedCredential = new AccessRegistry();
             $encryptedCredential->setUserCredential($credentialData['decrypted'],$user['email']);
             $encryptedCredential->setDescription($credentialData['description']);

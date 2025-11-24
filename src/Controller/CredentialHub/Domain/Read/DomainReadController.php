4a3f3f5a52d8519c7b949a8e3f1d9ac707d96f23
boszormenyirobert <boszormenyirobert@yahoo.com>
2025-11-24 01:10:43 +0100

    send encrypted credentials for decryption to the mobile

--- src/Controller/CredentialHub/Domain/Read/DomainReadController.php
+++ src/Controller/CredentialHub/Domain/Read/DomainReadController.php
@@ -115,12 +115,12 @@ class DomainReadController extends AbstractController
      * @param Request $request
      * @return JsonResponse
      */
-    #[Route('/credential/decrypted', name: 'domain_read_credential_decrypted', methods: "POST")]
+    #[Route('/credential/decrypted', name: 'domain_read_credential_encrypted', methods: "POST")]
     public function domainReadCredentialDecrypted(
         Request $request,
         DomainReadService $domainReadService
     ): JsonResponse {
-        $payloadKey = PayloadKeys::DOMAIN_READ_CREDENTIAL_DECRYPTED;
+        $payloadKey = PayloadKeys::DOMAIN_READ_CREDENTIAL_ENCRYPTED;
 
         try {
             $validatedPayload = $this->payloadValidator->validatePayload($request, $payloadKey);

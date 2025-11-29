cd3157902bdb905084b658cc95640dd864e8931c
boszormenyirobert <boszormenyirobert@yahoo.com>
2025-11-29 22:01:15 +0100

    add payload keys

--- src/Controller/CredentialHub/Vault/Read/VaultReadService.php
+++ src/Controller/CredentialHub/Vault/Read/VaultReadService.php
@@ -29,7 +29,7 @@ class VaultReadService
     public function getDecryptedCredentials(string $publicId): array{
         $applicationList = [];
         $getPages = $this->accessRegistryRepository->findBy(['publicId' => $publicId]);
-        $this->logger->critical('Encrypting application data for database storage.' . json_encode($getPages));
+        $this->logger->critical('Encrypting application data for database storage.' . json_encode($publicId));
 
         foreach ($getPages as $userPage) {
             if ($userPage->getApplication() !== null) {

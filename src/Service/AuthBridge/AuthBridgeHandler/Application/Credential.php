f1935b4c3cf16a6f6a8b3c5e64edfda23289b284
boszormenyirobert <boszormenyirobert@yahoo.com>
2025-11-29 23:27:16 +0100

    debug

--- src/Service/AuthBridge/AuthBridgeHandler/Application/Credential.php
+++ src/Service/AuthBridge/AuthBridgeHandler/Application/Credential.php
@@ -25,7 +25,7 @@ class Credential
         // deprecated
         // $userApplicationList = $this->listBuilder->buildDecryptedApplicationList($user['publicId'], $userSecret);
         $this->logger->critical("Setting decrypted values for application process ID: " . json_encode($user));
-        $userApplicationList = $user['credentials'];
+        $userApplicationList = $user['credentials']['credential'];
         
         $process = $this->authBridgeRepository->findOneBy([
             'applicationProcessId' => $user['applicationProcessId']

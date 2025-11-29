8cdf2bb9e1a5affea491d0c42de99a11ff5cdf54
boszormenyirobert <boszormenyirobert@yahoo.com>
2025-11-29 23:05:13 +0100

    add payload keys

--- src/Controller/CredentialHub/Vault/Read/VaultReadController.php
+++ src/Controller/CredentialHub/Vault/Read/VaultReadController.php
@@ -113,7 +113,7 @@ class VaultReadController extends AbstractController
             
             // User credentials by domain decryped by Database but enrcypted by the userSecret
             $response = $vaultReadService->getDecryptedCredentials($user['publicId']);
-            $this->logger->critical('Vault Read Decrypted Credentials for response: ' .response);
+            $this->logger->critical('Vault Read Decrypted Credentials for response: ' . json_encode($response));
             return $this->responseHelper->createSuccessResponse(['credentials' => $response]);
         } catch (\Exception $e) {
             return $this->responseHelper->handleException($e);

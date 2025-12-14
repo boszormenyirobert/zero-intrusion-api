1df90dd87578ba1c6551268f75491595a0339e45
boszormenyirobert <boszormenyirobert@yahoo.com>
2025-12-14 15:08:04 +0100

    fix typo

--- src/Controller/EmptyController.php
+++ src/Controller/EmptyController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class EmptyController
 {
     #[Route('/', name: 'empty')]

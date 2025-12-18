71abf0daf00534bc9c913ca86066dcda808a54f2
boszormenyirobert <boszormenyirobert@yahoo.com>
2025-12-18 16:01:54 +0100

    sanitize

--- src/Service/Crypters/CrypterDatabaseService.php
+++ src/Service/Crypters/CrypterDatabaseService.php
@@ -63,7 +63,7 @@ final class CrypterDatabaseService
         $decrypted->setCorporateIdSecret($this->decryptData($value->getCorporateIdSecret(), $iv));
         $decrypted->setSslPrivateKey($this->decryptData($value->getSslPrivateKey(), $iv));
         $decrypted->setSslPublicKey($value->getSslPublicKey());
-        $decrypted->setCallbackUserLogin($value->getCallbackUserLogin(), $iv);     
+        $decrypted->setCallbackUserLogin($value->getCallbackUserLogin());     
         $decrypted->setIv($value->getIv());   
 
         return $decrypted;

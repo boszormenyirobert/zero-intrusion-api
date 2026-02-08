61a90be897fce6d54f3b0f48b6b6e9c367375ed5
boszormenyirobert <boszormenyirobert@yahoo.com>
2026-02-08 16:10:43 +0100

    fix nfcEncryptionKey length

--- src/Entity/Identity.php
+++ src/Entity/Identity.php
@@ -46,7 +46,7 @@ class Identity
     #[ORM\Column(type: 'json', nullable: true)]
     private ?array $fcmToken = null;
 
-    #[ORM\Column(length: 300)]
+    #[ORM\Column(length: 3000)]
     private ?string $nfcEncryptionKey = null;
 
     public function __construct()
